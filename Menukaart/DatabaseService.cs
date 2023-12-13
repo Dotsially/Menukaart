307cbbb77b9844c64bd7cae0d91c80954557a03d
Yuichi Hezemans <61275852+Yuichi135@users.noreply.github.com>
2023-12-13 14:37:15 +0100

    Added database service

---
+++ Menukaart/DatabaseService.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SQLite;
+
+namespace Menukaart
+{
+    public class DatabaseService
+    {
+        public const string DatabaseFilename = "TestSQLite.db3";
+        private readonly SQLiteAsyncConnection _connection;
+
+        public DatabaseService()
+        {
+            _connection = new SQLiteAsyncConnection(Path.Combine(FileSystem.AppDataDirectory, DatabaseFilename));
+            _connection.CreateTableAsync<Session>();
+        }
+
+        public async Task<List<Session>> getSessions()
+        {
+            return await _connection.Table<Session>().ToListAsync();
+        }
+
+        public async Task<Session> getById(int id)
+        {
+            return await _connection.Table<Session>().Where(session => session.id == id).FirstOrDefaultAsync();
+        }
+
+        public async Task Create(Session session)
+        {
+            await _connection.InsertAsync(session);
+        }
+
+        public async Task Update(Session session)
+        {
+            await _connection.UpdateAsync(session);
+        }
+
+        public async Task Delete(Session session)
+        {
+            await _connection.DeleteAsync(session);
+        }
+    }
+}
---
+++ Menukaart/Session.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SQLite;
+
+namespace Menukaart
+{
+    [Table("session")]
+    public class Session
+    {
+        [PrimaryKey]
+        [AutoIncrement]
+        [Column("id")]
+        public int id { get; set;  }
+        [Column("date")]
+        public DateTime date { get; set; }
+        [Column("time")]
+        public int time { get; set; }
+        [Column("distance")]
+        public int distance { get; set; }
+    }
+}
