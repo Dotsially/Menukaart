756a7a6cf0704c537d0a4ae38bfe2e29551e656f
TeunGit <afm.lauwerijssen@student.avans.nl>
2023-12-14 19:29:43 +0100

    added route and sight class
    these classes hold information about routes and sights

---
+++ Menukaart/DataManagement/DataTypes/Route.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Menukaart.DataManagement.DataTypes
+{
+    internal class Class1
+    {
+    }
+}
---
+++ Menukaart/DataManagement/DataTypes/Sight.cs
@@ -0,0 +1,36 @@
+﻿using SQLite;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Menukaart.DataManagement.DataTypes
+{
+    
+    public class Sight
+    {
+        private int id { get; }
+        private string name { get; }
+        private string description { get; }
+        private string extraInfoURL { get; }
+        private Image image { get; }
+        private float longitude { get; }
+        private float latitude { get; }
+        private float altitude { get; }
+        
+
+        public Sight(int id, string name, string description, string extraInfoURL, Image image, float longitude, float latitude, float altitude) 
+        {
+            this.id = id;
+            this.name = name;
+            this.description = description;
+            this.extraInfoURL = extraInfoURL;
+            this.image = image;
+            this.longitude = longitude;
+            this.latitude = latitude;
+            this.altitude = altitude;
+        }
+       
+    }
+}
