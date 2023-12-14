68388a6ea60abd42518fe985c256c1eb6d1e3840
Kerr Beeldens <ksv.beeldens@student.avans.nl>
2023-12-14 17:32:35 +0100

    Started working on TutorialScreen carousel

--- Menukaart/ViewModel/TutorialPageViewModel.cs
+++ Menukaart/ViewModel/TutorialPageViewModel.cs
@@ -1,6 +1,21 @@
-﻿namespace Menukaart.ViewModel
+﻿using System.Collections.ObjectModel;
+using CommunityToolkit.Mvvm.ComponentModel;
+using Menukaart.Model;
+
+namespace Menukaart.ViewModel
 {
-    public class TutorialPageViewModel
+    public partial class TutorialPageViewModel : ObservableObject
     {
+        [ObservableProperty] private ObservableCollection<TutorialPageModel> _tutorialSections;
+
+        public TutorialPageViewModel()
+        {
+            _tutorialSections.Add( new()
+            {
+                Title = "Tutorial title",
+                Description = "Tutorial description",
+                ImageName = "vvv_logo.png"
+            });
+        }
     }
 }
