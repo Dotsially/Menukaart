914ce0f5369b93619aec7d0cfae55941f130ef5a
Moustapha Azaimi <m.azaimi@student.avans.nl>
2023-12-18 13:54:32 +0100

    Add places of interest hard-coded

---
+++ Menukaart/Model/POI.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Menukaart.Model
+{
+    internal class POI
+    {
+        public string Name;
+
+        public Location Location;
+
+        public POI(string name, Location location) 
+        {
+            this.Name = name;
+            this.Location = location;
+        }
+    }
+}
