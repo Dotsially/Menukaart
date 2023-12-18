3857f1c59675454b02f2b3e665e95f7cd4d96c7f
TeunGit <afm.lauwerijssen@student.avans.nl>
2023-12-18 18:08:04 +0100

    added listview to viewmodel

--- Menukaart/View/DatabasePageView.xaml.cs
+++ Menukaart/View/DatabasePageView.xaml.cs
@@ -1,70 +1,24 @@
 using System.Diagnostics;
 using Menukaart.DataManagement;
 using Menukaart.DataManagement.DataTypes;
+using Menukaart.ViewModel;
 
 namespace Menukaart.View;
 
 public partial class DatabasePageView : ContentPage
 {
-    private readonly DatabaseService _databaseService;
+    
 
 	public DatabasePageView(DatabaseService databaseService)
 	{
 		InitializeComponent();
-		_databaseService = databaseService;
+        BindingContext = new DatabasePageViewModel(databaseService, listView);
 
-        Task.Run(async () => { 
-            listView.ItemsSource = await _databaseService.getSessions();
-        });
     }
 
-    //Test code
-    private async Task CreateSession()
-    {
-        Session session = new Session();
-        await _databaseService.CreateSession(session);
-
-        session.AddSight(1);
-        session.AddSight(1);
-        session.AddSight(1);
-        await _databaseService.UpdateSession(session);
-        await _databaseService.UpdateSession(session);
-        session.AddSight(2);
-        await _databaseService.UpdateSession(session);
-        session.RemoveSight(1);
-        session.AddSight(3);
-        await _databaseService.UpdateSession(session);
-
-        await Task.Delay(1000);
-        List<Datalink> a = await _databaseService.getDatalinks();
-        foreach (var datalink in a)
-        {
-            Debug.WriteLine(datalink);
-        }
-    }
-    
-    // Test code
-    private async void CreateMultipleEmptySessions(int count)
-    {
-        List<Session> list = new List<Session>();
-        for (int i = 0; i < count; i++)
-        {
-            Session s = new Session()
-            {
-                id = i,
-                date = DateTime.Now.AddDays(i),
-                distance = i,
-                time = i
-            };
-
-            list.Add(s);
-        }
-
-        await _databaseService.CreateSessions(list);
-    }
-
-    private void Temp(object? sender, ItemTappedEventArgs e)
+    private void OpenSelectedItem(object? sender, ItemTappedEventArgs e)
     {
         throw new NotImplementedException();
     }
+
 }
---
+++ Menukaart/ViewModel/DatabasePageViewModel.cs
@@ -0,0 +1,80 @@
+﻿using Menukaart.DataManagement;
+using Menukaart.DataManagement.DataTypes;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Menukaart.ViewModel
+{
+    class DatabasePageViewModel
+    {
+        private readonly DatabaseService _databaseService;
+        
+        private ListView _listView;
+        
+        public DatabasePageViewModel(DatabaseService databaseService, ListView listview) 
+        {
+            _databaseService = databaseService;
+            _listView = listview;
+            CreateMultipleEmptySessions(10);
+            Task.Run(async () => {
+                _listView.ItemsSource = await _databaseService.getSessions();
+            });
+            
+        }
+
+        //Test code
+        private async Task CreateSession()
+        {
+            Session session = new Session();
+            await _databaseService.CreateSession(session);
+
+            session.AddSight(1);
+            session.AddSight(1);
+            session.AddSight(1);
+            await _databaseService.UpdateSession(session);
+            await _databaseService.UpdateSession(session);
+            session.AddSight(2);
+            await _databaseService.UpdateSession(session);
+            session.RemoveSight(1);
+            session.AddSight(3);
+            await _databaseService.UpdateSession(session);
+
+            await Task.Delay(1000);
+            List<Datalink> a = await _databaseService.getDatalinks();
+            foreach (var datalink in a)
+            {
+                Debug.WriteLine(datalink);
+                
+            }
+        }
+
+        // Test code
+        private async void CreateMultipleEmptySessions(int count)
+        {
+            List<Session> list = new List<Session>();
+            for (int i = 0; i < count; i++)
+            {
+                Session s = new Session()
+                {
+                    id = i,
+                    date = DateTime.Now.AddDays(i),
+                    distance = i,
+                    time = i
+                };
+
+                list.Add(s);
+            }
+
+            await _databaseService.CreateSessions(list);
+        }
+
+        private void OnItemClick(Session session) { 
+            Debug.WriteLine(session);
+        }
+    }
+}
