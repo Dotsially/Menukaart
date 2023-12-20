3ecff1052fc8e74ac08e4f4d7fb0d1ec1c548a35
Kerr Beeldens <ksv.beeldens@student.avans.nl>
2023-12-20 15:30:45 +0100

    Fixed the routelist

--- Menukaart/Model/RouteListPageModel.cs
+++ Menukaart/Model/RouteListPageModel.cs
@@ -3,9 +3,8 @@
     public class RouteListPageModel
     {
         public string Name { get; set; }
-
         public string Description { get; set; }
-
         public string ImageName { get; set; }
     }
+
 }
